a8c20d0e145466d78bb1c766e633a38ce24be5f2
Brendan Grady <cyhtan@gmail.com>
2015-06-17 18:02:23 -0400

    Resolve serving static asset issue

--- server/server.js
+++ server/server.js
@@ -1,22 +1,17 @@
-/*var express = require('express');
-var serveStatic = require('serve-static');
-
-var app = express();
-
-app.use(serveStatic('client', {'index': ['index.html', 'index.htm']}));
-app.listen(3000);*/
-
-
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', process.env.PORT || 3000 );
-app.use(express.static(__dirname + '/client'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use('/js',         express.static( path.join(__dirname, '../client/js') ));
+app.use('/components', express.static( path.join(__dirname, '../client/bower_components') ));
+app.use('/css',        express.static( path.join(__dirname, '../client/css') ));
+
+app.get('/',function(req,res){
+    res.sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+app.listen( app.get('port') , function() {
+    console.log('Running on port' + app.get('port'));
 });
